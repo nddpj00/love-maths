9451f382826db0a15e6722ca4f20da3a20da596f
nddpj00 <danieljones625@btinternet.com>
2022-09-05 20:27:55 +0000

    Add subract game code

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,6 +31,9 @@ function runGame(gameType) {
     }else if (gameType === "multiply") {
         displayMultiplyQuestion(num1, num2);
 
+    }else if (gameType === "subtract") {
+        displaySubtractQuestion(num1, num2);
+
     }else{
         alert (`unknown game type: ${gameType}`);
         throw `unknown game type: ${gameType}. Aborting!`;
@@ -79,6 +82,9 @@ function calculateCorrectAnswer(){
     }else if (operator === "x") {
         return [operand1 * operand2, "multiply"];
 
+    }else if (operator === "-") {
+        return [operand1 - operand2, "subract"];
+
     }else{
         alert(`unimplemented operator ${operator}`);
         throw `unimplemented operator ${operator}. Aborting!`;
@@ -108,10 +114,12 @@ function displayAdditionQuestion(operand1, operand2) {
     document.getElementById('operand1').textContent = operand1;
     document.getElementById('operand2').textContent = operand2;
     document.getElementById('operator').textContent = "+";
-
 }
 
-function displaySubtractQuestion() {
+function displaySubtractQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById('operand2').textContent = operand1 > operand2 ? operand2 : operand1;
+    document.getElementById('operator').textContent = "-";
 
 }
 
