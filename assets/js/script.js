ca6766d4d74a8c9d538ff56378aaecf266f7da2c
nddpj00 <danieljones625@btinternet.com>
2022-09-05 22:05:26 +0000

    save up to date

--- assets/js/script.js
+++ assets/js/script.js
@@ -149,5 +149,4 @@ function displayDivisionQuestion (operand1, operand2){
     document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
     document.getElementById('operand2').textContent = operand1 > operand2 ? operand2 : operand1;
     document.getElementById('operator').textContent = "/";
-
 }
